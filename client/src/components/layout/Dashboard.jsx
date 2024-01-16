9bbe12c30fbfff29f63c07e5bc43e52b98dcecc3
Piyush <Piyushhhagarwal@gmail.com>
2024-01-16 21:29:45 +0530

    Doctor service

--- client/src/components/layout/Dashboard.jsx
+++ client/src/components/layout/Dashboard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from '../ui/Header'
 
 function Dashboard() {
   return (
-    <div className='absolute left-[16%] top-0 z-10 h-[100dvh] w-[84%] flex justify-center items-center overflow-y-scroll text-stone-800'>
-        Route under prepare ..
+    <div className="absolute left-[16%] top-0 z-10 h-[100dvh] w-[84%] overflow-y-scroll">
+      <Header name="Dashboard" />
+
     </div>
   )
 }
--- client/src/components/ui/JustThere.jsx
+++ client/src/components/ui/JustThere.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { redirect } from 'react-router-dom';
 
 function JustThere() {
+
   return (
-    <div className='text-stone-800 w-full flex-col flex items-center justify-center'>
-      <img src="/doctor3.svg" alt="icon" className='block w-[24%] h-auto animate-[fadeIn_1s_ease-out_3s_forwards] duration-100'/>
+    <div className="clear-start flex w-full flex-col items-center justify-center text-stone-800">
+      <img
+        src="/doctor3.svg"
+        alt="icon"
+        className="block h-auto w-[24%] animate-[fadeIn_1s_ease-out_3s_forwards] duration-100"
+      />
 
       <span>Taking you to clinic ..</span>
     </div>
-  )
+  );
 }
 
-export default JustThere
+export default JustThere;
