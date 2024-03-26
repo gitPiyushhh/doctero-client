4f128cef1a6abf36b9de768e305d8a97cd5d71cd
Piyush Sultaniya <87389164+gitPiyushhh@users.noreply.github.com>
2024-03-26 18:11:40 +0530

    minor changes

--- client/src/components/ui/PatientDashboardHeader.jsx
+++ client/src/components/ui/PatientDashboardHeader.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 function PatientDashboardHeader() {
+  const user = JSON.parse(localStorage.getItem("user"));
 
   return (
     <div className='w-full bg-[#7C51C2] flex relative p-12 items-center rounded-lg'>
         <div className='flex flex-col'>
-        <span className='text-stone-50 text-[2rem] font-semibold'>Welcome back Piyush!</span>
+        <span className='text-stone-50 text-[2rem] font-semibold'>{`Welcome back ${user?.name}`}!</span>
         <span className='text-stone-50 text-[1rem] font-light'>Search the doctor you need</span>
         </div>
 
